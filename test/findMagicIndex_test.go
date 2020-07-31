79d196011948c8ff1d1a2076eed262db0f89e2b6
LinoSun <913896141@qq.com>
2020-07-31 19:18:07 +0800

    面试题0803魔术索引，添加测试模块

---
+++ test/findMagicIndex_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"goleetcode/others"
+	"testing"
+)
+
+func TestFindMagicIndex(t *testing.T) {
+	tests := []struct {
+		nums     []int
+		excepted int
+	}{
+		{[]int{1, 2, 3, 4}, -1},
+		{[]int{0, 2, 3, 4}, 0},
+		{[]int{1, 2, 3, 4}, -1},
+		{[]int{1, 2, 3, 4}, -1},
+		{[]int{1, 2, 3, 4}, -1},
+	}
+
+	for _, test := range tests {
+		if actual := others.FindMagicIndex(test.nums); actual != test.excepted {
+			t.Error("error")
+		}
+	}
+}
