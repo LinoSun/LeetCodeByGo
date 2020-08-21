c717a5982f4058f2db67d675774db3637b9fee00
LinoSun <913896141@qq.com>
2020-08-21 14:46:29 +0800

    111二叉树的最小深度

---
+++ TreeProblems/111_二叉树的最小深度.go
@@ -0,0 +1,44 @@
+package TreeProblems
+
+import "math"
+
+/*给定一个二叉树，找出其最小深度。
+
+最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例:
+
+给定二叉树 [3,9,20,null,null,15,7],
+
+3
+/ \
+9  20
+/  \
+15   7
+返回它的最小深度  2.*/
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil{
+		return 1
+	}
+	minD := math.MaxInt32
+	if root.Left != nil{
+		minD = min(minDepth(root.Left),minD)
+	}
+	if root.Right != nil{
+		minD = min(minDepth(root.Right),minD)
+	}
+	return minD + 1
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
