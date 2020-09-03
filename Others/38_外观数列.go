2b41af4c7906e86f9f5cc196ff0e446789387255
LinoSun <913896141@qq.com>
2020-09-03 22:46:51 +0800

    38外观数列

---
+++ Others/38_外观数列.go
@@ -0,0 +1,64 @@
+package Others
+
+import (
+	"strconv"
+	"strings"
+)
+
+/*给定一个正整数 n（1 ≤ n ≤ 30），输出外观数列的第 n 项。
+
+注意：整数序列中的每一项将表示为一个字符串。
+
+「外观数列」是一个整数序列，从数字 1 开始，序列中的每一项都是对前一项的描述。前五项如下：
+
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
+第一项是数字 1
+
+描述前一项，这个数是 1 即 “一个 1 ”，记作 11
+
+描述前一项，这个数是 11 即 “两个 1 ” ，记作 21
+
+描述前一项，这个数是 21 即 “一个 2 一个 1 ” ，记作 1211
+
+描述前一项，这个数是 1211 即 “一个 1 一个 2 两个 1 ” ，记作 111221
+
+ 
+
+示例 1:
+
+输入: 1
+输出: "1"
+解释：这是一个基本样例。
+示例 2:
+
+输入: 4
+输出: "1211"
+解释：当 n = 3 时，序列是 "21"，其中我们有 "2" 和 "1" 两组，"2" 可以读作 "12"，也就是出现频次 = 1 而 值 = 2；
+	类似 "1" 可以读作 "11"。所以答案是 "12" 和 "11" 组合在一起，也就是 "1211"。*/
+
+func countAndSay(n int) string {
+	str := "1"
+	for i := 2; i <= n; i++ {
+		var temp strings.Builder
+		preByte := str[0]
+		count := 1
+		for j := 1; j < len(str); j++ {
+			if str[j] == preByte {
+				count++
+			} else {
+				temp.WriteString(strconv.Itoa(count))
+				temp.WriteByte(preByte)
+				preByte = str[j]
+				count = 1
+			}
+		}
+		temp.WriteString(strconv.Itoa(count))
+		temp.WriteByte(preByte)
+		str = temp.String()
+	}
+	return str
+}
