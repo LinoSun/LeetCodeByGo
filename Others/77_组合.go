623493a8b96de9c129220bea9aa4ba41445a8731
LinoSun <913896141@qq.com>
2020-09-08 20:41:44 +0800

    77组合

--- Others/77_组合.go
+++ Others/77_组合.go
@@ -16,28 +16,26 @@ package Others
 ]*/
 
 
-func combine(n int, k int) (ans [][]int) {
-	temp := []int{}
-	var dfs func(int)
-	dfs = func(cur int) {
-		// 剪枝：temp 长度加上区间 [cur, n] 的长度小于 k，不可能构造出长度为 k 的 temp
-		if len(temp) + (n - cur + 1) < k {
+func combine(n int, k int) [][]int {
+	var ans [][]int
+
+	var dfs func(int, []int)
+	dfs = func(m int, path []int) {
+		if len(path) == k {
+			tmp := make([]int, k)
+			copy(tmp, path)
+			ans = append(ans, tmp)
 			return
 		}
-		// 记录合法的答案
-		if len(temp) == k {
-			comb := make([]int, k)
-			copy(comb, temp)
-			ans = append(ans, comb)
+		if m > n {
 			return
 		}
-		// 考虑选择当前位置
-		temp = append(temp, cur)
-		dfs(cur + 1)
-		temp = temp[:len(temp)-1]
-		// 考虑不选择当前位置
-		dfs(cur + 1)
+
+		dfs(m + 1, append(path, m))
+		dfs(m + 1, path)
+		return
 	}
-	dfs(1)
-	return
+
+	dfs(1, []int{})
+	return ans
 }
