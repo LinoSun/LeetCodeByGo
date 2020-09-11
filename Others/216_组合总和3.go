2c42fcac6a2659b8798d085c243bf53f8c5d99d3
LinoSun <913896141@qq.com>
2020-09-11 19:20:05 +0800

    216组合总和3

--- Others/216_组合总和3.go
+++ Others/216_组合总和3.go
@@ -1,6 +1,5 @@
 package Others
 
-
 /*找出所有相加之和为 n 的 k 个数的组合。组合中只允许含有 1 - 9 的正整数，并且每种组合中不存在重复的数字。
 
 说明：
@@ -16,6 +15,38 @@ package Others
 输入: k = 3, n = 9
 输出: [[1,2,6], [1,3,5], [2,3,4]]*/
 
+var comSumRes [][]int // 结果集
+var comSumList []int // 临时结果集
+
 func combinationSum3(k int, n int) [][]int {
-	111
+	/*
+	   回溯 + 剪枝 + 去重
+	   剪枝条件： > k 去掉
+	   去重条件： 加 start 控制
+	*/
+	comSumRes = make([][]int, 0)
+	comSumList = make([]int, 0)
+	backtarck(k, n, 1)
+	return comSumRes
+}
+
+func backtarck(k int, n int, start int) {
+	if n == 0 && k == 0 {
+		tmp := make([]int, len(comSumList))
+		copy(tmp, comSumList)
+		comSumRes = append(comSumRes, tmp)
+		return
+	}
+
+	for i:=start;i<=9;i++ {
+		if n - i < 0 {
+			break
+		}
+		if k == 1 && n-i > 0 {
+			continue
+		}
+		comSumList = append(comSumList, i)
+		backtarck(k-1, n-i, i+1)
+		comSumList = comSumList[:len(comSumList)-1]
+	}
 }
