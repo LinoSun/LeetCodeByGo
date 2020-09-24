d40db41b4158cb8119ac6dbdb5bbe31e2c087bfe
LinoSun <913896141@qq.com>
2020-09-24 20:32:38 +0800

    501二叉搜索树中的众数

---
+++ TreeProblems/501_二叉搜索树中的众数.go
@@ -0,0 +1,59 @@
+package TreeProblems
+/*给定一个有相同值的二叉搜索树（BST），找出 BST 中的所有众数（出现频率最高的元素）。
+
+假定 BST 有如下定义：
+
+结点左子树中所含结点的值小于等于当前结点的值
+结点右子树中所含结点的值大于等于当前结点的值
+左子树和右子树都是二叉搜索树
+例如：
+给定 BST [1,null,2,2],
+
+1
+\
+2
+/
+2
+返回[2].
+
+提示：如果众数超过1个，不需考虑输出顺序
+
+进阶：你可以不使用额外的空间吗？（假设由递归产生的隐式调用栈的开销不被计算在内）
+*/
+func findMode(root *TreeNode) (answer []int) {
+	var base, count, maxCount int
+	update := func(x int) {
+		if x == base {
+			count++
+		} else {
+			base, count = x, 1
+		}
+		if count == maxCount {
+			answer = append(answer, base)
+		} else if count > maxCount {
+			maxCount = count
+			answer = []int{base}
+		}
+	}
+	cur := root
+	for cur != nil {
+		if cur.Left == nil {
+			update(cur.Val)
+			cur = cur.Right
+			continue
+		}
+		pre := cur.Left
+		for pre.Right != nil && pre.Right != cur {
+			pre = pre.Right
+		}
+		if pre.Right == nil {
+			pre.Right = cur
+			cur = cur.Left
+		} else {
+			pre.Right = nil
+			update(cur.Val)
+			cur = cur.Right
+		}
+	}
+	return
+}
